94a6a737eb29f24f1ee9c13a13fa9a9ecc983c12
Tom-Goong <fall_for_you_ffm@163.com>
2022-04-16 17:01:12 +0800

    first commit

---
+++ src/test/java/com/yaoting/utf/UtfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaoting.utf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
